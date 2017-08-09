3f225a90ca2ebafcf0babd4e0941a2b08169d1f5
tragiccode <michael@tragiccode.com>
2017-08-09 12:49:25 -0500

    Added test for multiple roles

--- spec/classes/config_spec.rb
+++ spec/classes/config_spec.rb
@@ -88,5 +88,26 @@ lAGQAIABDAHIAeQBwAHQAbwBnAHIAYQBwAGgAaQBjACAAUAByAG8AdgBpAGQAZQByACAAdgAxAC4AMDC
       :logoutput => 'true',
     }).that_requires('Service[OctopusDeploy Tentacle]') }
   end
+  describe 'octopusdeploytentacle::config' do
+  context 'with roles => [web-server, database-server]' do
+    instance_pregenerated_certificate_thumbprint = "190FB29BA9A26E5A3565FCCE4B2191C15032709E"
+    instance_pregenerated_certificate = <<-HEREDOC
+lAGQAIABDAHIAeQBwAHQAbwBnAHIAYQBwAGgAaQBjACAAUAByAG8AdgBpAGQAZQByACAAdgAxAC4AMDCCAx8GCSqGSIb3DQEHAaCCAxAEggMMMIIDCDCCAwQGCyqGSIb3DQEMCgEDoIIC3DCCAtgGCiqGSIb3DQEJFgGgggLIBIICxDCCAsAwggGooAMCAQICEEEpdkMFV/6nQpDbY2Q5hccwDQYJKoZIhvcNAQEFBQAwGzEZMBcGA1UEAxMQT2N0b3B1cyBUZW50YWNsZTAgFw0xNzA0MjcwNTAwMDBaGA8yMTE3MDQyODA1MDAwMFowGzEZMBcGA1UEAxMQT2N0b3B1cyBUZW50YWNsZTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBALNp3b6awIWawXaZkFpt6AdeaOxPmNLrkKL+IJbTuuVc6vNjwec72jG/N7/gTHBb3zXafC9/05dbdhHOBYcGJEeZbspUkyhO/zQx2woBv7VwQ9GPXRQaVB2ZlTvQQqgDRwXwTJPl6MKBBMpN/3Fv2Px7+Ldkm4nGma18xkEdXczjU4p6b31RMR9XSr7lN/5157jKrXuPZQkvTW3ogFcVcczLly9ouYKH8wnnYgRZNtMpQ2HTPOlfLTl2VUj62i4DXX5Mo6yx/BAuzmbTS8K9+USweCBIKt1mO3n/cqmx3VGR/v0hkxEIORY/hzeXHjlQD0JCa2ZFazZHQVKyarvCduECAwEAATANBgkqhkiG9w0BAQUFAAOCAQEADocQt5dfdveBsm+Y+SOQF4HuqRedB4wMV7fSrzHU48a+krjn3Jtz4Al91VBPLU3GT+s387ZTfGaffiEyIr9DU2vYKQl73tbydsydSHDRMjDOfWssAKu/gQUwLV/hIWrcPTTh11d+R8ofngN7NdW6POUBlDH0KCQXrjPbcBHNQsGYCnkOankZinHHJhY+x3/Bu+IHwzaHNzLIpLoljhP9e7vQwDTowAz3WScL252AqS+/YrIolCsJEvaOBDFjpEUkD9yUqdJhkpmNfFGH8f0loP43r+lAGQAIABDAHIAeQBwAHQAbwBnAHIAYQBwAGgAaQBjACAAUAByAG8AdgBpAGQAZQByACAAdgAxAC4AMDCCAx8GCSqGSIb3DQEHAaCCAxAEggMMMIIDCDCCAwQGCyqGSIb3DQEMCgEDoIIC3DCCAtgGCiqGSIb3DQEJFgGgggLIBIICxDCCAsAwggGooAMCAQICEEEpdkMFV/6nQpDbY2Q5hccwDQYJKoZIhvcNAQEFBQAwGzEZMBcGA1UEAxMQT2N0b3B1cyBUZW50YWNsZTAgFw0xNzA0MjcwNTAwMDBaGA8yMTE3MDQyODA1MDAwMFowGzEZMBcGA1UEAxMQT2N0b3B1cyBUZW50YWNsZTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBALNp3b6awIWawXaZkFpt6AdeaOxPmNLrkKL+IJbTuuVc6vNjwec72jG/N7/gTHBb3zXafC9/05dbdhHOBYcGJEeZbspUkyhO/zQx2woBv7VwQ9GPXRQaVB2ZlTvQQqgDRwXwTJPl6MKBBMpN/3Fv2Px7+Ldkm4nGma18xkEdXczjU4p6b31RMR9XSr7lN/5157jKrXuPZQkvTW3ogFcVcczLly9ouYKH8wnnYgRZNtMpQ2HTPOlfLTl2VUj62i4DXX5Mo6yx/BAuzmbTS8K9+USweCBIKt1mO3n/cqmx3VGR/v0hkxEIORY/hzeXHjlQD0JCa2ZFazZHQVKyarvCduECAwEAATANBgkqhkiG9w0BAQUFAAOCAQEADocQt5dfdveBsm+Y+SOQF4HuqRedB4wMV7fSrzHU48a+krjn3Jtz4Al91VBPLU3GT+s387ZTfGaffiEyIr9DU2vYKQl73tbydsydSHDRMjDOfWssAKu/gQUwLV/hIWrcPTTh11d+R8ofngN7NdW6POUBlDH0KCQXrjPbcBHNQsGYCnkOankZinHHJhY+x3/Bu+IHwzaHNzLIpLoljhP9e7vQwDTowAz3WScL252AqS+/YrIolCsJEvaOBDFjpEUkD9yUqdJhkpmNfFGH8f0loP43r+
+    HEREDOC
 
+    let(:params) {{
+      :server_url                                   => 'http://deploy.tragiccode.com',
+      :api_key                                      => 'API-SDLKJFQWEIURLSMCN8348SDKJF',
+      :environment                                  => 'development',
+      :roles                                        => ['web-server', 'database-server'],
+      :server_thumbprint                            => 'CCCD736C25938806692F6C55521FA0869F29F280',
+      :instance_pregenerated_certificate            => instance_pregenerated_certificate,
+      :instance_pregenerated_certificate_thumbprint => instance_pregenerated_certificate_thumbprint,
+    }}
+
+    it { should contain_exec('register-octopustentacle-with-octopus-server').with({
+      :command   => 'C:\\Windows\\System32\\cmd.exe /c ""C:\\Program Files\\Octopus Deploy\\Tentacle\\tentacle.exe" register-with --instance "Tentacle" --server "http://deploy.tragiccode.com" --apiKey "API-SDLKJFQWEIURLSMCN8348SDKJF" --environment "development" --role web-server --role database-server --console"',
+    }) }
+  end
+  end
 end
