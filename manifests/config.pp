a1e516b5e31d912ad8778dd63a6153521d562529
tragiccode <michael@tragiccode.com>
2017-08-10 15:26:24 -0500

    WIP

--- manifests/config.pp
+++ manifests/config.pp
@@ -104,10 +104,10 @@ class octopusdeploytentacle::config(
                   & \"C:\\Program Files\\Octopus Deploy\\Tentacle\\tentacle.exe\" register-with --instance \"${instance_name}\" --server \"${server_url}\" --apiKey \"${api_key}\" --publicHostName \"${public_host_name}\" --environment \"${environment}\" --role ${command_line_roles} --console",
     unless    => "\$ErrorActionPreference = \"Stop\"
                   \$result = (Invoke-RestMethod -Method Get -Uri '${server_url}/api/Machines/all' -Headers @{\"X-Octopus-ApiKey\"=\"${api_key}\"}) | Where-Object { \$PSItem.Name -eq \"${host_name}\" }
-                   if (\$result.Count -eq 1) {
-                      Exit 0
+                   if (\$result -eq \$null) {
+                      Exit 1
                     }
-                   Exit 1",
+                   Exit 0",
     logoutput => true,
     provider  => 'powershell',
     require   => Service['OctopusDeploy Tentacle'],
